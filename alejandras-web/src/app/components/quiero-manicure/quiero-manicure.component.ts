0cb7fb0830894031282d0a8fe97d3b426db4fd11
Victor Rodriguez <vicrodriguezp@sodimac.cl>
2020-02-19 00:14:55 -0300

    implementacion envio de correo a servicio manicure

--- alejandras-web/src/app/components/quiero-manicure/quiero-manicure.component.ts
+++ alejandras-web/src/app/components/quiero-manicure/quiero-manicure.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { ManicureService } from '../../services/manicure.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { HttpService } from "../../services/http.service";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-quiero-manicure',
@@ -10,9 +13,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./quiero-manicure.component.css']
 })
 export class QuieroManicureComponent  {
+  loading = false;
+  buttionText = "Submit";
   manicure: any = {};
+  correo="virpeza91@gmail.com";
 
-  constructor(private activatedRoute: ActivatedRoute, private galeriaService: ManicureService, private modalService: NgbModal ) {
+  constructor(private activatedRoute: ActivatedRoute, private galeriaService: ManicureService,
+              private modalService: NgbModal, private http: HttpService ) {
 
     this.activatedRoute.params.subscribe(params => {
 
@@ -21,6 +28,33 @@ export class QuieroManicureComponent  {
 
     });
    }
+   register(forma: NgForm) {
+    this.loading = true;
+    this.buttionText = "Submiting...";
+    let user = {
+      nombre: forma.value.nombre,
+      telefono: forma.value.telefono,
+      servicio: this.manicure.nombre,
+      email: this.correo
+    }
+    console.log('nombnre usuario' + user);
+    this.http.sendMailContacto("http://localhost:3000/sendmailservicio", user).subscribe(
+      data => {
+        let res:any = data;
+        console.log(
+          `${user} is successfully register and mail has been sent and the message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+        this.loading = false;
+        this.buttionText = "Submit";
+      },() => {
+        this.loading = false;
+        this.buttionText = "Submit";
+      }
+    );
+  }
 
    verMensaje(modal) {
 
@@ -29,7 +63,7 @@ export class QuieroManicureComponent  {
 }
 guardar(forma: NgForm) {
 
-  console.log(forma)
+  console.log(forma);
 }
 
 }
