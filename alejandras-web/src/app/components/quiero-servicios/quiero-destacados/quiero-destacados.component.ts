439fdf96e4529366b5977f32c64a7f3e78987663
Victor Rodriguez <vicrodriguezp@sodimac.cl>
2020-03-02 18:44:25 -0300

    cambios destacados

--- alejandras-web/src/app/components/quiero-servicios/quiero-destacados/quiero-destacados.component.ts
+++ alejandras-web/src/app/components/quiero-servicios/quiero-destacados/quiero-destacados.component.ts
@@ -3,6 +3,7 @@ import { DestacadosService } from '../../../services/destacados.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { HttpService } from "../../../services/http.service";
 
 @Component({
   selector: 'app-quiero-destacados',
@@ -12,9 +13,12 @@ import { ActivatedRoute } from '@angular/router';
 export class QuieroDestacadosComponent  {
 
   galeriaDestacados: any = {};
+  loading = false;
+  buttionText = "Submit";
+  correo="alejandrsalon@gmail.com";
 
   constructor(private activatedRoute: ActivatedRoute, private destacadosService: DestacadosService,
-              private modalService: NgbModal ) {
+              private modalService: NgbModal, private http: HttpService ) {
 
     this.activatedRoute.params.subscribe(params => {
 
@@ -22,6 +26,33 @@ export class QuieroDestacadosComponent  {
 
     });
    }
+   register(forma: NgForm) {
+    this.loading = true;
+    this.buttionText = "Submiting...";
+    let user = {
+      nombre: forma.value.nombre,
+      telefono: forma.value.telefono,
+      servicio: 'Depilación de ' + this.galeriaDestacados.nombre,
+      email: this.correo
+    }
+    console.log('nombnre usuario' + user);
+    this.http.sendMailContacto("http://localhost:3000/sendmailservicio", user).subscribe(
+      data => {
+        let res:any = data;
+        console.log(
+          `${user} is successfully register and mail has been sent and the message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+        this.loading = false;
+        this.buttionText = "Submit";
+      },() => {
+        this.loading = false;
+        this.buttionText = "Submit";
+      }
+    );
+  }
 
    verMensaje(modal) {
 
