a2c11146f91b6cbcb90718e1a0566f3dfaffab04
Victor Rodriguez <vicrodriguezp@sodimac.cl>
2020-10-16 13:12:04 -0300

    cherry to master

--- alejandras-web/src/app/components/promociones/promociones.component.ts
+++ alejandras-web/src/app/components/promociones/promociones.component.ts
@@ -14,7 +14,12 @@ export class PromocionesComponent {
 
   loading = false;
   prueba = false;
+<<<<<<< HEAD
   cherry=true;
+=======
+  probandocherry=true;
+
+>>>>>>> 33256aa... probando cherry
   buttionText = "Submit";
   correo="contacto@alejandras.cl";
 
